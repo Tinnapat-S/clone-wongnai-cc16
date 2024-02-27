e4c2894e723d1d5b5d90d070b4913f828f6b04a8
Tinnapt-S <tinnapat.l@gmail.com>
2024-02-27 16:52:38 +0700

    try-to-seed

--- prisma/seed.js
+++ prisma/seed.js
@@ -1,15 +1,17 @@
 const { PrismaClient, Role } = require("@prisma/client")
 const prisma = new PrismaClient()
+const provinces = require("./data/provinces.json")
+const districts = require("./data/districts.json")
+const priceLength = require("./data/priceLength.json")
+const subDistricts = require("./data/subDistricts.json")
 
 async function seeding() {
-    await prisma.user.create({
-        data: {
-            username: "admin",
-            password: "admin1234",
-            role: Role.ADMIN,
-            firstName: "admin",
-            lastName: "cc16",
-        },
+    try {
+    } catch (err) {
+        console.log(err)
+    }
+    await prisma.priceLength.createMany({
+        data: priceLength,
     })
 }
 seeding()
