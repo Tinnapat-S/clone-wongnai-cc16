76900cc0102124a6146a32c81fb435dfa6a98f83
Weeraphat2000 <weeraphat.main@gmail.com>
2024-03-06 15:30:27 +0700

    test chat

--- src/app.js
+++ src/app.js
@@ -18,6 +18,51 @@ const server = http.createServer(app)
 
 restApiServer(app)
 
+const cors = require("cors")
+const { Server } = require("socket.io")
+const chat = express()
+const chatServer = http.createServer(chat)
+const io = new Server(chatServer, {
+    cors: {
+        origin: ["http://localhost:5173"],
+        credentials: true,
+        methods: ["GET", "POST"],
+    },
+})
+chat.use(cors())
+// io.on("connection", (socket) => {
+//     console.log("a user connected")
+// })
+// const onlineUser = {}
+// io.use((socket, next) => {
+//     const userId = socket.handshake.auth.sender
+//     onlineUser[userId] = socket.id
+//     next()
+// })
+// io.on("connection", (socket) => {
+//     console.log(onlineUser, "check online user")
+//     socket.on("message", (msg) => {
+//         // database
+//         // io.emit("received", { msg, ...socket.handshake.auth })
+//         io.to([onlineUser["1"], onlineUser["2"]]).emit("received", { msg })
+//         console.log(socket.handshake.auth)
+//         // ได้ message ออกมา
+//         // console.log(msg)
+//         // console.log(socket.handshake.auth)
+//         // return socket.handshake.auth
+//     })
+
+//     console.log("a user connected")
+//     socket.on("disconnect", () => {
+//         console.log("user disconnected")
+//     })
+// })
+// chat.get("/", () => {
+//     console.log("first")
+// })
+// // chat.post
+// chatServer.listen(8888, () => console.log(8888, "port"))
+
 //=====================================================Listening Zone
 console.log(`API DOCS ON:  http://${host}:${port}/docs`)
 server.listen(+port, host, () => {
