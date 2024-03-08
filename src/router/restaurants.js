c98cc240cc5d47eef7d714789a60331f5029b125
rnixer <kewell700@hotmail.com>
2024-03-08 19:52:25 +0700

    merchantAdd_delete

--- src/router/restaurants.js
+++ src/router/restaurants.js
@@ -3,6 +3,7 @@ const express = require("express")
 const c = require("../controller")
 const authenticate = require("../middlewares/authenticate")
 const getUser = require("../middlewares/getUser")
+const upload = require("../middlewares/upload")
 
 const restaurantRoute = express.Router()
 
@@ -10,5 +11,7 @@ restaurantRoute.get("/", c.restaurants.getAll)
 restaurantRoute.get("/filter?", getUser, c.restaurants.getFilter)
 restaurantRoute.get("/bookmark", authenticate, c.restaurants.getRestaurantsWithUser)
 restaurantRoute.get("/:id", c.restaurants.getRestaurantById)
+restaurantRoute.post("/Img/:restaurantId", upload.fields([{ name: "img" }]), c.restaurants.uploadRestaurantImg)
+restaurantRoute.delete("/Img/:id", c.restaurants.deleteRestaurantImg)
 
 module.exports = restaurantRoute
